5a49b042ccb9f390abb04a7b050d80c1a15420b7
Nick <nickmandev@gmail.com>
2017-06-10 19:06:41 +0300

    Feature: Comments now have responses.

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -6,8 +6,23 @@
     <div class="video-comments-section">
       <form v-on:submit.prevent>
         <textarea v-model="body"></textarea>
+        <p v-if="error">{{ error }}</p>
         <button v-on:click="postComment">Submit</button>
       </form>
+      <div v-if="comments" class="comments">
+        <div class="comment" v-for="comment in comments">
+          <p>{{ comment.user_info }} {{ comment.created_at | dateParser }} </p>
+          <p>{{ comment.body }}</p>
+          <p v-on:click="toogleResponseForm()">Add comment</p>
+          <div v-if="showResponseForm" class="comments-reponse-form">
+            <form v-on:submit.prevent>
+              <textarea v-model="responseBody"></textarea>
+              <p v-if="error">{{ error }}</p>
+              <button v-on:click="postCommentResponse(comment)">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,11 +39,15 @@
       return {
         data: {},
         videoData: {},
+        comments: [],
         name: '',
         url480p: '',
         url720p: '',
         player: '',
         body: '',
+        responseBody: '',
+        error: '',
+        showResponseForm: false,
       }
     },
     beforeCreate() {
@@ -36,10 +55,14 @@
       let token = localStorage.getItem('token');
       this.$http.get(`videos/${id}`).then((response) => {
         this.data['video'] = response.data.video;
+        this.comments = response.data.comments;
         this.url480p = `${this.data['video'].url}480/index.m3u8`
         this.url720p = `${this.data['video'].url}720/index.m3u8`
         this.videoData = JSON.parse(this.data.video.video_data);
         this.$nextTick(() => {
+          this.comments.forEach((comment) => {
+            comment.user_info = JSON.parse(comment.user_info);
+          });
           this.setPlayer();
         });
       });
@@ -66,9 +89,25 @@
         })
       },
       postComment() {
-        this.$http.post('comments', { comment: {'body': this.body, 'video_id': this.data['video'].id} }).then((response) => {
+        this.$http.post('comments', { comment: { 'body': this.body, 'video_id': this.data['video'].id } }).then((response) => {
+          if (response.body.comment) {
+            let comment = response.body.comment
+            comment['user_info'] = JSON.parse(comment['user_info'])
+            this.comments.push(comment)
+          }
+
+          if (response.body.error) {
+            this.error = response.body.error
+          }
+        })
+      },
+      postCommentResponse(comment) {
+        this.$http.post('comments_response', { comments_response: { 'body': this.responseBody, 'comment_id': comment.id} }).then((response) => {
           console.log(response);
         })
+      },
+      toogleResponseForm() {
+        this.showResponseForm = !this.showResponseForm;
       }
     },
   }
